f94343aa6e14d45eae6119c91c5e1cc8920c9f4e
Sihan W <61381713+WWWH123Q@users.noreply.github.com>
2026-05-10 16:19:49 +0800

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,4 @@
-"""启动脚本"""
+
 
 import uvicorn
 from app.config import get_settings
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     settings = get_settings()
     
     uvicorn.run(
-        "app.api.main:app",  #去 app.api.main 里面找到 app，然后把它跑起来。
+        "app.api.main:app",  
         host=settings.host,
         port=settings.port,
         reload=True,
-        log_level=settings.log_level.lower()  #日志，
+        log_level=settings.log_level.lower() 
     )
 
